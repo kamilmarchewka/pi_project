c21b62616c1120df3be3d1abe609c11e2ac3c6f3
Kamil <kamilm2003@gmail.com>
2023-12-08 11:01:16 +0100

    fix: compiling issue

--- main.cpp
+++ main.cpp
@@ -7,61 +7,30 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 500
 
-using namespace sf;
-
 int main(){
-  Ball pilka_golfowa(10, 10, 20); // Create ball object
-  std::cout << "Polozenie pilki: " << pilka_golfowa.getX() << ", " << pilka_golfowa.getY() << std::endl; // Print ball position
-  
-  RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "MiniGolf"); // Define window
+
+  // Define window
+  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "MiniGolf");
   window.setFramerateLimit(60); // Set limit to 60 FPS
 
-  RectangleShape rect; // Define rectangle
-  Vector2f position(10,10);
-  Vector2f size(100, 100);
-  rect.setSize(size); // Set size of rectangle
-  rect.setPosition(position); // Set position of rectangle
-
-  int xVelocity = 5;
-  int yVelocity = 15;
-
-  Texture texture;
-  texture.loadFromFile("assets/main_bg.png");
-  texture.setSmooth(true);
-  Sprite sprite;
-  sprite.setTexture(texture);
-  sprite.setPosition(0, WINDOW_HEIGHT - texture.getSize().y);
+  sf::Texture main_bg_t; // Texture for main bg
+  main_bg_t.loadFromFile("assets/main_bg.png");
+  main_bg_t.setSmooth(true); // Smothen edges
+  sf::Sprite main_bg_s; // Sprite for main bg
+  main_bg_s.setTexture(main_bg_t); // Set spirte texture
+  // align to bottom left
+  main_bg_s.setPosition(0, WINDOW_HEIGHT - main_bg_t.getSize().y);
 
   while(window.isOpen()) // Game loop
   {
-    Event event; // Define variable which holds events data
+    sf::Event event; // Define variable which holds events data
     while(window.pollEvent(event)) // Check if there is any event
     { // Check if event is window closed
-      if(event.type == Event::Closed) window.close(); // Close window
+      if(event.type == sf::Event::Closed) window.close(); // Close window
     }
 
-    position.x += xVelocity;
-    position.y += yVelocity;
-    rect.setPosition(position);
-
-    // Detect collision in X axis with window borders
-    if(
-      position.x <=0 || // Left border
-      position.x + size.x >= WINDOW_WIDTH // Right border
-    ){
-      xVelocity *= -1;
-    }
-    // Detect collision in Y axis with window borders
-    if(
-      position.y <= 0 || // Top border
-      position.y + size.y >= WINDOW_HEIGHT // Bottom border
-    ){
-      yVelocity *= -1;
-    }
-
-    window.clear(Color(168,202,89,100)); // Clear window with black color
-    window.draw(sprite);
-    window.draw(rect); // Draw rectangle
+    window.clear(sf::Color(168,202,89,100)); // Clear window with green
+    window.draw(main_bg_s);
     window.display(); // Display window
   }
   
