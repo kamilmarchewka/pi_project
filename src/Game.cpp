cbbf29890ec913fc5df3e76b506d28f3318eaca4
Kamil <kamilm2003@gmail.com>
2023-12-28 15:50:16 +0100

    Usuniecie pozostalosci po wczesniejszym ladowaniu poziomow

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,18 +2,6 @@
 #include "Game.h"
 #include "GameplayScreen.h"
 
-extern int lvl1;
-extern int lvl1Strokes;
-extern int lvl1Map[8][16];
-//----------------- 2 ---------------
-extern int lvl2;
-extern int lvl2Strokes;
-extern int lvl2Map[8][16];
-//----------------- 3 ---------------
-extern int lvl3;
-extern int lvl3Strokes;
-extern int lvl3Map[8][16];
-
 void Game::initAssets()
 {
     // Ladowanie textur
